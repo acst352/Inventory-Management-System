0dbc6500fe7e5d837f3253c70b91592644e86323
Alex <acst352@gmail.com>
2025-05-20 17:10:34 -0600

    Estilizing Edit page

--- RPInventarios/Pages/Marcas/Edit.cshtml.cs
+++ RPInventarios/Pages/Marcas/Edit.cshtml.cs
@@ -1,77 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Marcas
-{
-    public class EditModel : PageModel
-    {
-        private readonly RPInventarios.Data.InventariosContext _context;
+namespace RPInventarios.Pages.Marcas;
 
-        public EditModel(RPInventarios.Data.InventariosContext context)
+public class EditModel : PageModel
+{
+    private readonly InventariosContext _context;
+
+    public EditModel(InventariosContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Marca Marca { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        [BindProperty]
-        public Marca Marca { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
+        var marca = await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
+        if (marca == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
+        Marca = marca;
+        return Page();
+    }
 
-            var marca =  await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
-            if (marca == null)
-            {
-                return NotFound();
-            }
-            Marca = marca;
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more information, see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        _context.Attach(Marca).State = EntityState.Modified;
+
+        try
         {
-            if (!ModelState.IsValid)
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!MarcaExists(Marca.Id))
             {
-                return Page();
+                return NotFound();
             }
-
-            _context.Attach(Marca).State = EntityState.Modified;
-
-            try
+            else
             {
-                await _context.SaveChangesAsync();
+                throw;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MarcaExists(Marca.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
         }
 
-        private bool MarcaExists(int id)
-        {
-            return _context.Marcas.Any(e => e.Id == id);
-        }
+        return RedirectToPage("./Index");
+    }
+
+    private bool MarcaExists(int id)
+    {
+        return _context.Marcas.Any(e => e.Id == id);
     }
 }
+
