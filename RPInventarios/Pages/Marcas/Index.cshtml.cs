0f4bf9dc16e6d5a15df89db2413b1e73ddfac1e4
Alex <acst352@gmail.com>
2025-05-20 16:36:28 -0600

    Refactor - pluralizing Marca - Set List of Marcas to ReadOnly to improve performance

--- RPInventarios/Pages/Marcas/Index.cshtml.cs
+++ RPInventarios/Pages/Marcas/Index.cshtml.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Marcas
+namespace RPInventarios.Pages.Marcas;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly InventariosContext _context;
+
+    public IndexModel(InventariosContext context)
     {
-        private readonly RPInventarios.Data.InventariosContext _context;
+        _context = context;
+    }
 
-        public IndexModel(RPInventarios.Data.InventariosContext context)
-        {
-            _context = context;
-        }
+    public IReadOnlyList<Marca> Marcas { get; set; } = default!;
 
-        public IList<Marca> Marca { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            Marca = await _context.Marcas.ToListAsync();
-        }
+    public async Task OnGetAsync()
+    {
+        Marcas = await _context.Marcas.ToListAsync();
     }
 }
