4be9edf98645a3393415d2fe09835fa7aca599c7
Alex <acst352@gmail.com>
2025-05-20 17:24:59 -0600

    Estilizing Details page

--- RPInventarios/Pages/Marcas/Details.cshtml.cs
+++ RPInventarios/Pages/Marcas/Details.cshtml.cs
@@ -1,43 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Marcas
+namespace RPInventarios.Pages.Marcas;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly InventariosContext _context;
+
+    public DetailsModel(InventariosContext context)
     {
-        private readonly RPInventarios.Data.InventariosContext _context;
+        _context = context;
+    }
 
-        public DetailsModel(RPInventarios.Data.InventariosContext context)
+    public Marca Marca { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
-        }
-
-        public Marca Marca { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var marca = await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (marca is not null)
-            {
-                Marca = marca;
-
-                return Page();
-            }
-
             return NotFound();
         }
+
+        var marca = await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (marca is not null)
+        {
+            Marca = marca;
+
+            return Page();
+        }
+
+        return NotFound();
     }
 }
