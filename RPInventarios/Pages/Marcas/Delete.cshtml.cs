49fd7d1e1bd5dd180773863e387e56bcb9975757
Alex <acst352@gmail.com>
2025-05-20 17:31:46 -0600

    Estilizing Delete page

--- RPInventarios/Pages/Marcas/Delete.cshtml.cs
+++ RPInventarios/Pages/Marcas/Delete.cshtml.cs
@@ -1,62 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Marcas
+namespace RPInventarios.Pages.Marcas;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly InventariosContext _context;
+
+    public DeleteModel(InventariosContext context)
     {
-        private readonly RPInventarios.Data.InventariosContext _context;
+        _context = context;
+    }
 
-        public DeleteModel(RPInventarios.Data.InventariosContext context)
+    [BindProperty]
+    public Marca Marca { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
-        }
-
-        [BindProperty]
-        public Marca Marca { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var marca = await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (marca is not null)
-            {
-                Marca = marca;
-
-                return Page();
-            }
-
             return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        var marca = await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (marca is not null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            Marca = marca;
 
-            var marca = await _context.Marcas.FindAsync(id);
-            if (marca != null)
-            {
-                Marca = marca;
-                _context.Marcas.Remove(Marca);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            return Page();
         }
+
+        return NotFound();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var marca = await _context.Marcas.FindAsync(id);
+        if (marca != null)
+        {
+            Marca = marca;
+            _context.Marcas.Remove(Marca);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
