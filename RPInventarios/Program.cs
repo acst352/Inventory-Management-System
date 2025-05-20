350610a0979d984844d7766e1f6379208f5d4b31
Alex <acst352@gmail.com>
2025-05-20 15:19:38 -0600

    Ensure DB exists

--- RPInventarios/Program.cs
+++ RPInventarios/Program.cs
@@ -26,7 +26,14 @@ else
     app.UseMigrationsEndPoint();
 }
 
-    app.UseHttpsRedirection();
+// Asegura que la BD exista y esté lista para usarse. Útil para entorno de desarrollo y pruebas. En producción se debe usar migraciones.
+using (var scope = app.Services.CreateScope()) // Crear un ámbito de servicios.
+{
+    var services = scope.ServiceProvider; // Permite acceder a los servicios registrados en el contenedor de dependencias.
+    var context = services.GetRequiredService<InventariosContext>(); // Recupera el contexto de la BD configurado para la app.
+    context.Database.EnsureCreated(); // Verifica si la BD existe, si no, la crea automáticamente según el modelo definido.
+}
+app.UseHttpsRedirection();
 
 app.UseRouting();
 
