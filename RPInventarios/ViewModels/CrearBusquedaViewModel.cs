9024f6d49ba4dd2b1055f7a08814811a09db2223
Alex <acst352@gmail.com>
2025-05-21 17:54:29 -0600

    Implement Partial View for CREAR & BUSQUEDA - Fix registred marca counter

---
+++ RPInventarios/ViewModels/CrearBusquedaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RPInventarios.ViewModels;
+
+public class CrearBusquedaViewModel
+{
+    public string TerminoBusqueda { get; set; }
+    public int TotalRegistros { get; set; }
+    public string TituloCrear { get; set; }
+}
+
+// Clase que será el modelo de la vista parcial para "Crear Marca" y "Buscar Marca"
