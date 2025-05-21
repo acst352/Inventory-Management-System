6e21e0b00c1029100b0b12c0e1c86353be8dcf8f
Alex <acst352@gmail.com>
2025-05-21 11:02:50 -0600

    Add DataAnnotations

--- RPInventarios/Models/Marca.cs
+++ RPInventarios/Models/Marca.cs
@@ -1,7 +1,14 @@
-﻿namespace RPInventarios.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RPInventarios.Models;
+
 public class Marca
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "La marca es requerida")]
+    [MinLength(5, ErrorMessage = "La marca debe tener al menos 5 caracteres")]
+    [MaxLength(50, ErrorMessage = "La marca no puede tener más de 50 caracteres")]
+    [Display(Name = "Marca")]
     public string Nombre { get; set; }
     public virtual ICollection<Producto> Productos { get; set; }
     // Se usa ICollection<Producto> para representar la relación 1:N entre
