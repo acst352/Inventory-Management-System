60bdbb3ea9b4b6d486bf5d45a6ae82086b5293da
Alex <acst352@gmail.com>
2025-05-22 17:03:38 -0600

    refactor marca page model

--- RPInventarios/Pages/Marcas/Index.cshtml.cs
+++ RPInventarios/Pages/Marcas/Index.cshtml.cs
@@ -18,30 +18,34 @@ public class IndexModel : PageModel
     }
 
     public List<Marca> Marcas { get; set; } = default!;
+    // Propiedades de Búsqueda
     [BindProperty(SupportsGet = true)]
-    public int? Pagina { get; set; }
-    [BindProperty(SupportsGet = true)] // Permite enlazar la propiedad TerminoBusqueda desde la URL
     public string TerminoBusqueda { get; set; }
     public int TotalRegistros { get; set; }
+    //Propiedades de Paginación
+    [BindProperty(SupportsGet = true)]
+    public int? Pagina { get; set; }
     public int TotalPaginas { get; set; }
+
     public async Task OnGetAsync()
     {
-        // Paginación manual usando Skip y Take con EF 
-        var registrosPorPagina = _configuration.GetValue("RegistrosPorPagina", 10);
+        // Búsqueda o Filtrado
         var consulta = _context.Marcas.AsNoTracking();
 
-        // Filtrar por nombre de marca
         if (!string.IsNullOrEmpty(TerminoBusqueda))
         {
             consulta = consulta.Where(m => m.Nombre.Contains(TerminoBusqueda));
         }
 
         TotalRegistros = await consulta.CountAsync();
+
+        // Paginación
         var numeroPagina = Pagina ?? 1;
+        var registrosPorPagina = _configuration.GetValue("RegistrosPorPagina", 10);
         TotalPaginas = (int)Math.Ceiling((double)TotalRegistros / registrosPorPagina);
 
         Marcas = await consulta
-            .OrderBy(m => m.Id)
+            .OrderBy(d => d.Id)
             .Skip((numeroPagina - 1) * registrosPorPagina)
             .Take(registrosPorPagina)
             .ToListAsync();
