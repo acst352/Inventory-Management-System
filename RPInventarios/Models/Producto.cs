dab36b49bce575cd3afcb3c196b071ad273f9040
Alex <acst352@gmail.com>
2025-05-26 13:55:34 -0600

    Set textbox's character length

--- RPInventarios/Models/Producto.cs
+++ RPInventarios/Models/Producto.cs
@@ -17,13 +17,14 @@ public class Producto
     [MaxLength(50, ErrorMessage = "El nombre del producto no puede tener más de 50 caracteres")]
     public string Nombre { get; set; } = string.Empty;
     [Display(Name = "Descripcion")]
-    [StringLength(200, MinimumLength = 5, ErrorMessage = "La descripcion del producto debe tener minimo 5 caracteres y masimo 200")]
+    [StringLength(200, MinimumLength = 5, ErrorMessage = "La descripcion del producto debe tener minimo 5 caracteres y /*máximo*/ 200")]
     public string Descripcion { get; set; } = string.Empty;
     [Display(Name = "Marca")]
     [Required(ErrorMessage = "La marca es requerida")]
     public int MarcaId { get; set; }
     public virtual Marca? Marca { get; set; }
     [Required(ErrorMessage = "El costo es requerido")]
+    [Range(0, 9999999999, ErrorMessage = "El costo no puede tener más de 10 dígitos")]
     public decimal Costo { get; set; }
     public EstatusProducto Estatus { get; set; } = EstatusProducto.Activo;
 }
