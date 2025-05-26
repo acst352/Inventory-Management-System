07ad3ba95da4d0307b31daa78c8f3d68c0119daa
Alex <acst352@gmail.com>
2025-05-26 15:46:40 -0600

    Cleaning code

--- RPInventarios/Pages/Productos/Delete.cshtml.cs
+++ RPInventarios/Pages/Productos/Delete.cshtml.cs
@@ -1,62 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Productos
+namespace RPInventarios.Pages.Productos;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly InventariosContext _context;
+
+    public DeleteModel(InventariosContext context)
     {
-        private readonly RPInventarios.Data.InventariosContext _context;
+        _context = context;
+    }
 
-        public DeleteModel(RPInventarios.Data.InventariosContext context)
+    [BindProperty]
+    public Producto Producto { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
-        }
-
-        [BindProperty]
-        public Producto Producto { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var producto = await _context.Productos.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (producto is not null)
-            {
-                Producto = producto;
-
-                return Page();
-            }
-
             return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        var producto = await _context.Productos.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (producto is not null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            Producto = producto;
 
-            var producto = await _context.Productos.FindAsync(id);
-            if (producto != null)
-            {
-                Producto = producto;
-                _context.Productos.Remove(Producto);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            return Page();
         }
+
+        return NotFound();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+
+
+        var producto = await _context.Productos.FindAsync(id);
+        if (producto != null)
+        {
+            Producto = producto;
+            _context.Productos.Remove(Producto);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
