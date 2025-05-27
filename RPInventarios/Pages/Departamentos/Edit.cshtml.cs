a262d2920ee5c3d0ecbc04dd1c980c59c58a36ef
Alex <acst352@gmail.com>
2025-05-27 17:21:02 -0600

    Add Department EDIT Notification

--- RPInventarios/Pages/Departamentos/Edit.cshtml.cs
+++ RPInventarios/Pages/Departamentos/Edit.cshtml.cs
@@ -1,77 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Departamentos
-{
-    public class EditModel : PageModel
-    {
-        private readonly RPInventarios.Data.InventariosContext _context;
+namespace RPInventarios.Pages.Departamentos;
 
-        public EditModel(RPInventarios.Data.InventariosContext context)
+public class EditModel : PageModel
+{
+    private readonly InventariosContext _context;
+    private readonly INotyfService _servicioNotificacion;
+
+    public EditModel(InventariosContext context, INotyfService servicioNotificacion)
+    {
+        _context = context;
+        _servicioNotificacion = servicioNotificacion;
+    }
+
+    [BindProperty]
+    public Departamento Departamento { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            _servicioNotificacion.Warning("El ID del departamento debe tener un valor no nulo");
+            return NotFound();
         }
 
-        [BindProperty]
-        public Departamento Departamento { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
+        var departamento = await _context.Departamentos.FirstOrDefaultAsync(m => m.Id == id);
+        if (departamento == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            _servicioNotificacion.Warning($"No se encontró el departamento con ID {id}");
+            return NotFound();
+        }
+        Departamento = departamento;
+        return Page();
+    }
 
-            var departamento =  await _context.Departamentos.FirstOrDefaultAsync(m => m.Id == id);
-            if (departamento == null)
-            {
-                return NotFound();
-            }
-            Departamento = departamento;
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more information, see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            _servicioNotificacion.Error($"Error al editar el departamento {Departamento.Nombre}");
+            return Page();
+        }
+        /* Validar si ya existe un departamento con el mismo nombre al intentar editar un departamento */
+        var existeDepartamentoBd = _context.Marcas.Any(u => u.Nombre.ToLower().Trim() == Departamento.Nombre.ToLower().Trim() && u.Id != Departamento.Id);
+        if (existeDepartamentoBd)
+        {
+            _servicioNotificacion.Warning($"Ya existe un departamento con el nombre {Departamento.Nombre}");
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        _context.Attach(Departamento).State = EntityState.Modified;
+
+        try
         {
-            if (!ModelState.IsValid)
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!DepartamentoExists(Departamento.Id))
             {
-                return Page();
+                return NotFound();
             }
-
-            _context.Attach(Departamento).State = EntityState.Modified;
-
-            try
+            else
             {
-                await _context.SaveChangesAsync();
+                throw;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DepartamentoExists(Departamento.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
         }
 
-        private bool DepartamentoExists(int id)
-        {
-            return _context.Departamentos.Any(e => e.Id == id);
-        }
+        _servicioNotificacion.Success($"Departamento {Departamento.Nombre} editado correctamente");
+        return RedirectToPage("./Index");
+    }
+
+    private bool DepartamentoExists(int id)
+    {
+        return _context.Departamentos.Any(e => e.Id == id);
     }
 }
