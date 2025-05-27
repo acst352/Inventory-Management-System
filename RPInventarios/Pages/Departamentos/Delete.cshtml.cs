fa501144988272dcaf7b2f529a596897ee15f710
Alex <acst352@gmail.com>
2025-05-27 16:40:11 -0600

    Add Department DELETE Notification

--- RPInventarios/Pages/Departamentos/Delete.cshtml.cs
+++ RPInventarios/Pages/Departamentos/Delete.cshtml.cs
@@ -1,62 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
 
-namespace RPInventarios.Pages.Departamentos
+namespace RPInventarios.Pages.Departamentos;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly InventariosContext _context;
+    private readonly INotyfService _servicioNotificacion;
+
+    public DeleteModel(InventariosContext context, INotyfService servicioNotificacion)
     {
-        private readonly RPInventarios.Data.InventariosContext _context;
+        _context = context;
+        _servicioNotificacion = servicioNotificacion;
+    }
 
-        public DeleteModel(RPInventarios.Data.InventariosContext context)
+    [BindProperty]
+    public Departamento Departamento { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
-        }
-
-        [BindProperty]
-        public Departamento Departamento { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var departamento = await _context.Departamentos.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (departamento is not null)
-            {
-                Departamento = departamento;
-
-                return Page();
-            }
-
+            _servicioNotificacion.Warning("El ID del departamento debe tener un valor");
             return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        var departamento = await _context.Departamentos.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (departamento is not null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            Departamento = departamento;
 
-            var departamento = await _context.Departamentos.FindAsync(id);
-            if (departamento != null)
-            {
-                Departamento = departamento;
-                _context.Departamentos.Remove(Departamento);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            return Page();
         }
+
+        _servicioNotificacion.Warning($"No se encontró el departamento con ID {id}");
+        return NotFound();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            _servicioNotificacion.Warning("El ID del departamento debe tener un valor");
+            return NotFound();
+        }
+
+        var departamento = await _context.Departamentos.FindAsync(id);
+        if (departamento != null)
+        {
+            Departamento = departamento;
+            _context.Departamentos.Remove(Departamento);
+            await _context.SaveChangesAsync();
+            _servicioNotificacion.Success($"Departamento {Departamento.Nombre} eliminado correctamente");
+        }
+        else
+        {
+            _servicioNotificacion.Warning($"No se encontró el departamento con ID {id}");
+            //return NotFound();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
