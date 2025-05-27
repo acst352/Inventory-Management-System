a0db67898e5306c03d6c05bb42d68239ae1f7466
Alex <acst352@gmail.com>
2025-05-27 17:39:26 -0600

    Update costo validation

--- RPInventarios/Models/Producto.cs
+++ RPInventarios/Models/Producto.cs
@@ -26,7 +26,7 @@ public class Producto
     public virtual Marca? Marca { get; set; }
     [Required(ErrorMessage = "El costo del producto es obligatorio.")]
     [Range(1, 9999999999, ErrorMessage = "El costo debe estar entre 1 y 9.999.999.999.")]
-    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "El costo debe ser un número positivo, permitiendo hasta dos decimales.")]
+    [RegularExpression(@"^\d{1,10}(\.\d{1,2})?$", ErrorMessage = "El costo debe ser un número positivo, permitiendo hasta dos decimales y sin letras.")]
     public decimal Costo { get; set; }
     public EstatusProducto Estatus { get; set; } = EstatusProducto.Activo;
 }
