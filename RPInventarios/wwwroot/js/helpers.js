a0231034930bcc4db0a9cbac170f898cae330eca
Alex <acst352@gmail.com>
2025-05-28 10:02:46 -0600

    Refactor: centralize reusable script in helpers.js

---
+++ RPInventarios/wwwroot/js/helpers.js
@@ -0,0 +1,9 @@
+﻿// Limit user input to only numbers
+function soloNumeros(inputSelector) {
+    var input = document.querySelector(inputSelector);
+    if (input) {
+        input.addEventListener('input', function () {
+            this.value = this.value.replace(/[^0-9]/g, '');
+        });
+    }
+}
