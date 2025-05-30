1cd7b2091b956efdd116949af8dc1022479ad2b5
Alex <acst352@gmail.com>
2025-05-30 16:20:22 -0600

    .

--- RPInventarios/Pages/Productos/Edit.cshtml.cs
+++ RPInventarios/Pages/Productos/Edit.cshtml.cs
@@ -55,8 +55,6 @@ public class EditModel : PageModel
         return Page();
     }
 
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more information, see https://aka.ms/RazorPagesCRUD.
     public async Task<IActionResult> OnPostAsync()
     {
         if (!ModelState.IsValid)
