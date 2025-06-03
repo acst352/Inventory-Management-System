be5ce600401211d929752c2f8f623ed6278cc35f
Alex <acst352@gmail.com>
2025-06-03 13:40:47 -0600

    Avoid presicion data loss defining decimals

--- RPInventarios/Models/Producto.cs
+++ RPInventarios/Models/Producto.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace RPInventarios.Models;
 
 public enum EstatusProducto
@@ -28,6 +29,7 @@ public class Producto
     [Required(ErrorMessage = "El costo del producto es obligatorio.")]
     [Range(1, 9999999999, ErrorMessage = "El costo debe estar entre 1 y 9.999.999.999.")]
     [RegularExpression(@"^\d{1,10}(\.\d{1,2})?$", ErrorMessage = "El costo debe ser un número positivo, permitiendo hasta dos decimales y sin letras.")]
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Costo { get; set; }
     //public EstatusProducto Estatus { get; set; } = EstatusProducto.Activo;
     public EstatusProducto Estatus { get; set; } = EstatusProducto.Inactivo;
