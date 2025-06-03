c9d17bdbbeb3c03b1542f5279baea213189b3a79
Alex <acst352@gmail.com>
2025-06-03 13:40:09 -0600

    Add new migration - Ensure theres no null values before changing the column Description

---
+++ RPInventarios/Migrations/20250603193503_ModeloInicial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RPInventarios.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModeloInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Asegura que no haya valores nulos antes de cambiar la columna
+            migrationBuilder.Sql("UPDATE Producto SET Descripcion = '' WHERE Descripcion IS NULL;");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Producto",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Producto",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
