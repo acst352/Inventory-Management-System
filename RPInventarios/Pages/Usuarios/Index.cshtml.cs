97afd8684493e5f2cb16f80c34d3d8db35505266
Alex <acst352@gmail.com>
2025-06-04 10:50:11 -0600

    Avance

--- RPInventarios/Pages/Usuarios/Index.cshtml.cs
+++ RPInventarios/Pages/Usuarios/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RPInventarios.Data;
 using RPInventarios.Models;
@@ -8,17 +9,60 @@ namespace RPInventarios.Pages.Usuarios;
 public class IndexModel : PageModel
 {
     private readonly InventariosContext _context;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(InventariosContext context)
+    public IndexModel(InventariosContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
-    public IList<Usuario> Usuario { get; set; } = default!;
+    public List<Usuario> Usuarios { get; set; } = default!;
+    // Propiedades de Búsqueda
+    [BindProperty(SupportsGet = true)]
+    public string? TerminoBusqueda { get; set; }
+    public int TotalRegistros { get; set; }
+    //Propiedades de Paginación
+    [BindProperty(SupportsGet = true)]
+    public int? Pagina { get; set; }
+    public int TotalPaginas { get; set; }
+    // Propiedades de Ordenamiento por ID y Nombre
+    [BindProperty(SupportsGet = true)]
+    public string? Orden { get; set; }
+    [BindProperty(SupportsGet = true)]
+    public string? Direccion { get; set; }
 
     public async Task OnGetAsync()
     {
-        Usuario = await _context.Usuarios
-            .Include(u => u.Perfil).ToListAsync();
+        // Búsqueda o Filtrado
+        var consulta = _context.Usuarios.AsNoTracking();
+
+        if (!string.IsNullOrEmpty(TerminoBusqueda))
+        {
+            consulta = consulta.Where(m => m.Nombre.Contains(TerminoBusqueda));
+        }
+
+        TotalRegistros = await consulta.CountAsync();
+
+        // Ordenamiento dinámico por ID y Nombre
+        string orden = Orden ?? "Id";
+        string direccion = Direccion ?? "desc";
+        consulta = (orden, direccion) switch
+        {
+            ("Nombre", "asc") => consulta.OrderBy(p => p.Nombre),
+            ("Nombre", "desc") => consulta.OrderByDescending(p => p.Nombre),
+            ("Id", "desc") => consulta.OrderByDescending(p => p.Id),
+            _ => consulta.OrderBy(p => p.Id)
+        };
+
+        // Paginación
+        var numeroPagina = Pagina ?? 1;
+        var registrosPorPagina = _configuration.GetValue("RegistrosPorPagina", 10);
+        TotalPaginas = (int)Math.Ceiling((double)TotalRegistros / registrosPorPagina);
+
+        Usuarios = await consulta
+            .Skip((numeroPagina - 1) * registrosPorPagina)
+            .Take(registrosPorPagina)
+            .ToListAsync();
     }
 }
