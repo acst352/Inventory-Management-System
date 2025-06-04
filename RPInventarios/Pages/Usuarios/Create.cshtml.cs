e577b9f150d43eaaac03fc37d8a57be4a28825c5
Alex <acst352@gmail.com>
2025-06-04 17:23:03 -0600

    Add some creation validations

--- RPInventarios/Pages/Usuarios/Create.cshtml.cs
+++ RPInventarios/Pages/Usuarios/Create.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,16 +11,18 @@ namespace RPInventarios.Pages.Usuarios;
 public class CreateModel : PageModel
 {
     private readonly InventariosContext _context;
+    private readonly INotyfService _servicioNotificacion;
 
-    public CreateModel(InventariosContext context)
+    public CreateModel(InventariosContext context, INotyfService servicioNotificacion)
     {
         _context = context;
+        _servicioNotificacion = servicioNotificacion;
     }
 
     private void CargarListas()
     {
         // Perfil de usuario
-        ViewData["PerfilId"] = _context.Perfiles 
+        ViewData["PerfilId"] = _context.Perfiles
             .AsNoTracking()
             .Select(m => new SelectListItem
             {
@@ -30,21 +33,31 @@ public class CreateModel : PageModel
 
     public IActionResult OnGet()
     {
-        ViewData["PerfilId"] = new SelectList(_context.Perfiles, "Id", "Nombre"); 
+        ViewData["PerfilId"] = new SelectList(_context.Perfiles, "Id", "Nombre");
         CargarListas();
         return Page();
     }
 
     [BindProperty]
     public Usuario Usuario { get; set; } = default!;
+    public SelectList Perfiles { get; set; }
     public async Task<IActionResult> OnPostAsync()
     {
         if (!ModelState.IsValid)
         {
+            CargarListas();
+            _servicioNotificacion.Error($"Es necesario corregir los problemas para poder crear el usuario {Usuario.Username}");
             return Page();
         }
 
-        _context.Usuarios.Add(Usuario);
+        var existeUsuarioBd = _context.Usuarios.Any(u => u.Username.ToLower().Trim() == Usuario.Username.ToLower().Trim());
+        if (existeUsuarioBd)
+        {
+            CargarListas();
+            _servicioNotificacion.Warning($"Ya existe un usuario con la cuenta {Usuario.Username}");
+            return Page();
+        }
+
         await _context.SaveChangesAsync();
 
         return RedirectToPage("./Index");
